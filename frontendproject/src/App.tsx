8d102be8625926417333c4c9cc38de44664827af
William Tracy <afishionado@gmail.com>
2024-04-24 22:10:57 -0700

    Add connect button!

--- frontendproject/src/App.tsx
+++ frontendproject/src/App.tsx
@@ -1,7 +1,33 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultConfig,
+  RainbowKitProvider,
+  ConnectButton
+} from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
+import {
+  mainnet,
+  polygon,
+  optimism,
+  arbitrum,
+  base,
+} from 'wagmi/chains';
+import {
+  QueryClientProvider,
+  QueryClient,
+} from "@tanstack/react-query";
+
 import './App.css'
+
+const config = getDefaultConfig({
+  appName: 'My RainbowKit App',
+  projectId: 'YOUR_PROJECT_ID',
+  chains: [mainnet, polygon, optimism, arbitrum, base],
+  ssr: false
+});
+const queryClient = new QueryClient();
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,26 +41,13 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+      <RainbowKitProvider>
+      <ConnectButton />
+      </RainbowKitProvider>
+      </QueryClientProvider>
+      </WagmiProvider>
     </>
   )
 }
