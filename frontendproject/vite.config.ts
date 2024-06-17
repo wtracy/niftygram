118085d8948cab02b62605c6468077afa22ea1ca
William Tracy <afishionado@gmail.com>
2024-06-17 01:21:38 -0700

    Work with multiple HTML files.

--- frontendproject/vite.config.ts
+++ frontendproject/vite.config.ts
@@ -4,6 +4,14 @@ import path, {resolve} from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        app: resolve(__dirname, 'app.html')
+      }
+    }
+  },
   plugins: [react()],
   resolve: {
     alias: {
